5bd218bcf81f000005a659a15bea2648a159e3cf
szuwgh <895340293@qq.com>
2022-01-16 15:28:16 +0000

    save

--- src/cgroups/v1/manager.rs
+++ src/cgroups/v1/manager.rs
@@ -2,7 +2,9 @@ use procfs::process::Process;
 
 pub struct Manager {}
 
-impl Manager {}
+impl Manager {
+    fn new(container_id: &str) -> Manager {}
+}
 
 pub fn get_subsystem_mount_point() {
     println!("{:?}", Process::myself().unwrap().mountinfo().unwrap());
