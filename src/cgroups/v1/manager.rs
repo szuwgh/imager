8180513a47e47bf27724fb8226954276deedd944
szuwgh <895340293@qq.com>
2022-01-17 19:13:42 +0800

    add cgroup code

--- src/cgroups/v1/manager.rs
+++ src/cgroups/v1/manager.rs
@@ -1,11 +1,67 @@
+use super::subsystem::{SubSystemType, SUBSYSTEMLIST};
+use anyhow::{anyhow, bail, Result};
 use procfs::process::Process;
 
-pub struct Manager {}
+use std::{collections::HashMap, path::PathBuf};
 
-impl Manager {
-    fn new(container_id: &str) -> Manager {}
+pub struct CgroupsManager {
+    subsystems: HashMap<SubSystemType, PathBuf>,
 }
 
-pub fn get_subsystem_mount_point() {
-    println!("{:?}", Process::myself().unwrap().mountinfo().unwrap());
+impl CgroupsManager {
+    pub fn new(container_id: &str) -> CgroupsManager {
+        for subsystem in SUBSYSTEMLIST {
+            Self::get_subsystem_path(&subsystem);
+        }
+        Self {
+            subsystems: HashMap::new(),
+        }
+    }
+
+    fn add_task() {}
+
+    fn get_subsystem_path(subsystem: &SubSystemType) {
+        let mount_point = get_subsystem_mount_point(subsystem).unwrap();
+        println!("mount_point: {:?}", mount_point);
+    }
+}
+
+// 当要向某个 CGroup 加入 Thread 时，将Thread PID 写入 tasks 或 cgroup.procs 即可，cgroup.procs 会自动变更为该 Task 所属的 Proc PID。
+// 如果要加入 Proc 时，则只能写入到 cgroup.procs 文件(未解)，tasks 文件会自动更新为该 Proc 下所有的 Thread PID。
+// 可以通过cat /proc/PID/cgroup查看某个 Proc/Thread 的 CGroup 信息
+pub fn get_subsystem_mount_point(subsystem: &SubSystemType) -> Result<PathBuf> {
+    let subsystem = subsystem.to_string();
+    Process::myself()?
+        .mountinfo()?
+        .into_iter()
+        .find(|m| {
+            println!("mount_point: {:?}", m.mount_point);
+            println!("fs_type: {:?}", m.fs_type);
+            if m.fs_type == "cgroup" {
+                // Some systems mount net_prio and net_cls in the same directory
+                // other systems mount them in their own diretories. This
+                // should handle both cases.
+                if subsystem == "net_cls" {
+                    return m.mount_point.ends_with("net_cls,net_prio")
+                        || m.mount_point.ends_with("net_prio,net_cls")
+                        || m.mount_point.ends_with("net_cls");
+                } else if subsystem == "net_prio" {
+                    return m.mount_point.ends_with("net_cls,net_prio")
+                        || m.mount_point.ends_with("net_prio,net_cls")
+                        || m.mount_point.ends_with("net_prio");
+                }
+
+                if subsystem == "cpu" {
+                    return m.mount_point.ends_with("cpu,cpuacct")
+                        || m.mount_point.ends_with("cpu");
+                }
+                if subsystem == "cpuacct" {
+                    return m.mount_point.ends_with("cpu,cpuacct")
+                        || m.mount_point.ends_with("cpuacct");
+                }
+            }
+            m.mount_point.ends_with(&subsystem)
+        })
+        .map(|m| m.mount_point)
+        .ok_or_else(|| anyhow!("could not find mountpoint for {}", subsystem))
 }
---
+++ src/cgroups/v1/subsystem.rs
@@ -0,0 +1,72 @@
+use std::fmt::Display;
+// root@vm:~# lssubsys -a
+// cpuset
+// cpu
+// cpuacct
+// blkio
+// memory
+// devices
+// freezer
+// net_cls
+// perf_event
+// net_prio
+// hugetlb
+// pids
+// rdma
+// misc
+#[derive(Hash, PartialEq, Eq, Debug, Clone)]
+pub enum SubSystemType {
+    CpuSet,
+    Cpu,
+    CpuAcct,
+    Blkio,
+    Memory,
+    Devices,
+    Freezer,
+    NetCls,
+    PerfEvent,
+    NetPrio,
+    HugeTlb,
+    Pids,
+    Rdma,
+    Misc,
+}
+
+impl Display for SubSystemType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let print = match *self {
+            Self::CpuSet => "cpuset",
+            Self::Cpu => "cpu",
+            Self::CpuAcct => "cpuacct",
+            Self::Blkio => "blkio",
+            Self::Memory => "memory",
+            Self::Devices => "pids",
+            Self::Freezer => "perf_event",
+            Self::NetCls => "memory",
+            Self::PerfEvent => "blkio",
+            Self::NetPrio => "freezer",
+            Self::HugeTlb => "freezer",
+            Self::Pids => "freezer",
+            Self::Rdma => "freezer",
+            Self::Misc => "freezer",
+        };
+        write!(f, "{}", print)
+    }
+}
+
+pub const SUBSYSTEMLIST: &[SubSystemType] = &[
+    SubSystemType::CpuSet,
+    SubSystemType::Cpu,
+    SubSystemType::CpuAcct,
+    SubSystemType::Blkio,
+    SubSystemType::Memory,
+    SubSystemType::Devices,
+    SubSystemType::Freezer,
+    SubSystemType::NetCls,
+    SubSystemType::PerfEvent,
+    SubSystemType::NetPrio,
+    SubSystemType::HugeTlb,
+    SubSystemType::Pids,
+    SubSystemType::Rdma,
+    SubSystemType::Misc,
+];
